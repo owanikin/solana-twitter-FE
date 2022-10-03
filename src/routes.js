cae0b98dc9a38b3cda5785016f0f725f236a54e2
Owanikin <oderindeife@gmail.com>
2022-10-03 12:48:54 +0100

    scaffolding the frontend

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,32 @@
 export default [
     {
-        name: "Home",
-        path: "/",
+        name: 'Home',
+        path: '/',
         component: require('@/components/PageHome').default,
     },
     {
-        name: "Topics",
-        path: "/topics/:topic?",
-        component: require("@/components/PageTopics").default,
+        name: 'Topics',
+        path: '/topics/:topic?',
+        component: require('@/components/PageTopics').default,
     },
     {
-        name: "Profile",
-        path: "/profile",
+        name: 'Users',
+        path: '/users/:author?',
+        component: require('@/components/PageUsers').default,
+    },
+    {
+        name: 'Profile',
+        path: '/profile',
         component: require('@/components/PageProfile').default,
     },
     {
-        name: "NotFound",
-        path: "/:pathMatch(.*)*",
-        component: require('@/components/PageHome').default,
+        name: 'Tweet',
+        path: '/tweet/:tweet',
+        component: require('@/components/PageTweet').default,
+    },
+    {
+        name: 'NotFound',
+        path: '/:pathMatch(.*)*',
+        component: require('@/components/PageNotFound').default,
     },
 ]
